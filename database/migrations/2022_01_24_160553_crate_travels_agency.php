f06a1317b6a3caeac20f71e0e627d8d455f9d2bf
stefanofilippini <filippini.stefano96@gmail.com>
2022-01-24 18:03:01 +0100

    created table

--- database/migrations/2022_01_24_160553_crate_travels_agency.php
+++ database/migrations/2022_01_24_160553_crate_travels_agency.php
@@ -13,7 +13,16 @@ class CrateTravelsAgency extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('travel_name', 100);
+            $table->string('destination', 100);
+            $table->string('description', 1000);
+            $table->integer('price')->unsigned();
+            $table->integer('duration')->unsigned();
+            $table->boolean('is_available')->default(1);
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +32,6 @@ class CrateTravelsAgency extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('travels');
     }
 }
